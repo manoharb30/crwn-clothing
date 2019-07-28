ea792b3012bc4c217f604f456fd20656ac7e9466
manoharb30 <manohar.hello@gmail.com>
2019-07-28 21:57:23 +0530

    added styles

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,41 +1,11 @@
 import React from 'react';
 import './homepage.styles.scss';
+import Directory from '../components/directory/Directory'
 
 const Homepage = () => {
     return (
         <div className = 'homepage'>
-            <div className = 'directory-menu'>
-                <div className = 'menu-item'>
-                    <div className = 'content'>
-                        <h1 className = 'title'>HATS</h1>
-                        <span className = 'subtitle'>SHOP NOW </span>
-                   </div>
-                </div>
-                <div className = 'menu-item'>
-                    <div className = 'content'>
-                        <h1 className = 'title'>JACKETS</h1>
-                        <span className = 'subtitle'>SHOP NOW</span>
-                   </div>
-                </div>
-                <div className = 'menu-item'>
-                    <div className = 'content'>
-                        <h1 className = 'title'>Sneakers</h1>
-                        <span className = 'subtitle'>SHOP NOW</span>
-                   </div>
-                </div>
-                <div className = 'menu-item'>
-                    <div className = 'content'>
-                        <h1 className = 'title'>Womens</h1>
-                        <span className = 'subtitle'>SHOP NOW</span>
-                   </div>
-                </div>
-                <div className = 'menu-item'>
-                    <div className = 'content'>
-                        <h1 className = 'title'>Mens</h1>
-                        <span className = 'subtitle'>SHOP NOW</span>
-                   </div>
-                </div>
-            </div>
+           <Directory />
         </div>
     );
 };
